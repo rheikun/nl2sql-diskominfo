5e85db806e25047895a0ae023652bb57b064821c
rheikun <rheisanfrs24@gmail.com>
2025-05-02 14:36:32 +0700

    fix(answer_generator): fix instruction that given to model

--- modules/answer_generator.py
+++ modules/answer_generator.py
@@ -3,20 +3,25 @@ from langchain.chains import LLMChain
 from model_llm import ModelLLM
 
 def create_answer_generator(memory=None):
-    llm = ModelLLM(model="gemma3", temperature=0.7)
+    llm = ModelLLM(model="gemma3", temperature=0.65)
     
     prompt = PromptTemplate(
         input_variables=["user_question", "query_result"],
-        template="""Anda adalah Generator Jawaban NL2SQL yang dirancang untuk memberikan jawaban yang jelas dan ringkas dalam **bahasa Indonesia** berdasarkan hasil query SQL.
+        template="""
+        Anda adalah Generator Jawaban NL2SQL yang dirancang untuk memberikan jawaban yang jelas dan ringkas dalam **bahasa Indonesia** berdasarkan hasil query SQL.
         Tugas Anda adalah menganalisis hasil query SQL yang dihasilkan oleh Generator NL2SQL dan menyajikan informasi dalam kalimat yang ramah pengguna.
-        Instruksi:
+        
+        ## Instruksi:
         1. Tinjau pertanyaan pengguna untuk memahami konteks dan maksudnya.
         2. Analisis hasil query SQL untuk mengekstrak informasi yang relevan.
-        3. Sajikan jawaban dalam **bahasa Indonesia** dengan tata bahasa yang benar dan gaya bahasa yang ramah pengguna.
-        4. Jangan gunakan istilah teknis seperti "SQL", "query", atau "database" dalam jawaban.
-        5. Jangan beri sintaks SQL ke pengguna. 
-        6. Jika ada permintaan yang bersifat destruktif, seperti ""INSERT", "UPDATE", atau "DELETE", tolak permintaan tersebut dengan sopan.
-        7. Jika mengandung informasi pribadi seperti KTP, Email, atau Nomor Telepon, jangan tampilkan informasi pribadi tersebut.
+        3. Sajikan jawaban dalam **bahasa Indonesia** dengan tata bahasa yang benar sesuai dengan penulisan dan gaya bahasa yang ramah pengguna.
+        4. Gunakan istilah formal seperti "jurusan" untuk program studi (hindari penggunaan kata "jurus").
+        5. Jangan gunakan istilah teknis seperti "SQL", "query", atau "database" dalam jawaban.
+        6. Jangan beri sintaks SQL ke pengguna. 
+        7. Jika ada permintaan yang bersifat destruktif, seperti ""INSERT", "UPDATE", atau "DELETE", tolak permintaan tersebut dengan sopan.
+        8. Jika mengandung informasi pribadi seperti KTP, Email, atau Nomor Telepon, jangan tampilkan informasi pribadi tersebut.
+        
+        ## Contoh:
         Contoh 1: 
         - Pertanyaan: Siapa penduduk laki-laki yang bekerja di bidang teknologi?
         - Jawaban: "Berikut adalah penduduk laki-laki yang bekerja di bidang teknologi:
